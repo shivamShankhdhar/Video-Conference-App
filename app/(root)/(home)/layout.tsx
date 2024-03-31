199d89efebb7269c7ff28e97241b039f48fea56f
shivamShankhdhar <s.shankhdhar1981@gmail.com>
2024-03-31 09:17:04 +0530

    hero section of home page is ready

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -8,7 +8,7 @@ const HomeLayout = ({ children }: { children: ReactNode; }) => {
       <NavBar />
       <div className="flex pt-20">
         <SideBar />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6  max-md:pb-14 sm:px-14">
           <div className="w-full">
             {children}
           </div>
--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
 const Homepage = () => {
-  return (
-    <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-2xl font-bold'>
-        Home
-      </h1>
+  // let months =
+  //   ['January', 'February', 'March', 'April',
+  //     'May', 'June', 'July', 'August',
+  //     'September', 'October', 'November', 'December'];
+  // const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
+  const now = new Date();
+  const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' })
+  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now)
+
+  return (
+    <section className='flex size-full flex-col gap-10 top-0 text-white'>
+      <div className="h-[250px] w-full bg-hero rounded-[20px] bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>Upcoming Meeting at: 12:30 PM </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className='text-5xl font-extrabold lg:text-5xl'>
+              {time}
+            </h1>
+            <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{date}</p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
