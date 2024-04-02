b463ac3cb219b7761eb106f23b33ce0aac8512b9
shivamShankhdhar <s.shankhdhar1981@gmail.com>
2024-04-02 09:23:25 +0530

    personal meeting room is ready

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,13 +1,78 @@
+// @ts-nocheck
+'use client'
+import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
+import { useGetCallById } from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs'
+import { useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useRouter } from 'next/navigation';
+import { title } from 'process';
 import React from 'react'
+const Table = ({ title, description }: { title: string; description: string }) => (
+  <div className='flex flex-col items-start gap-2 xl:flex-row'>
+    <h1 className='text-base font-medium text-sky-1 lg:text-xl xl:min-w-32'>{title}</h1>
+    <h1 className='truncate text-sm font-bold max-sm:max-w-[320px]'>{description}</h1>
+  </div>
+)
 
-const page = () => {
+const PersonalRoom = () => {
+  const router = useRouter()
+  const client = useStreamVideoClient();
+  const { user } = useUser();
+  const meetingId = user.id
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`
+  const { call } = useGetCallById(meetingId);
+
+  const startRoom = async () => {
+
+    if (!client || !user) return;
+
+    const newCall = client.call('default', meetingId)
+
+    await newCall.getOrCreate({
+      data: {
+        starts_at: new Date().toISOString(),
+      }
+    })
+    toast({ title: "Meeting about to start in just a second please hold on !" })
+    router.push(`/meeting/${meetingId}?personal=true`)
+  }
+
   return (
-    <section className='flex size-full flex-col gap-10 text-white'>
+    <section className='flex size-full flex-col gap-10 text-white' >
       <h1 className='text-2xl font-bold'>
         Personal Room
       </h1>
-    </section>
+      <div className="flex w-full flex-col gap-8 xl:max-w-[900px]">
+        <Table
+          title='Topic'
+          description={`${user?.username ? user?.username : "Personal Room"}'s meeting room`}
+        />
+
+        <Table
+          title='Meeting ID'
+          description={`${meetingId}`}
+        />
+
+        <Table
+          title='Invite Link'
+          description={`${meetingLink}`}
+        />
+      </div>
+      <div className="flex gap-5">
+        <Button className="bg-blue-1" onClick={startRoom}>
+          Start Meeting
+        </Button>
+
+        <Button className="bg-dark-3" onClick={() => {
+          navigator.clipboard.writeText(meetingLink)
+          toast({ title: "Link Copied" })
+        }}>
+          Copy Meeting Link
+        </Button>
+      </div>
+    </section >
   )
 }
 
-export default page
+export default PersonalRoom
