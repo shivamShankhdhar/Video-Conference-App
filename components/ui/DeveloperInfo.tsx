eaf83d34b9dd455a2c209b1216ce3c8fc4f5ab51
shivamShankhdhar <s.shankhdhar1981@gmail.com>
2024-04-02 10:47:51 +0530

    developer info updated

--- components/ui/DeveloperInfo.tsx
+++ components/ui/DeveloperInfo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DeveloperInfo = () => {
   return (
-    <div className='h-[20px] text-white bg-dark-4 text-center'>
+    <div className='h-[30px] py-1 text-white text-sm bg-dark-4 text-center'>
       Designed and developed by Er. Shivam Shankhdhar
     </div>
   )
